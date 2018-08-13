24808c0012098e277895b886591e1559607bd156
kuk <rnrrudvy@outlook.kr>
2018-08-14 07:42:24 +0900

    # 종료버튼 추가 # 다른 함수로 entry변수 넘겨서 get하면 값 받아옴 # get은 현재 entry에 있는 값을 받아오는 것 # 키보드를 눌렀을 때랑 Enter쳤을 때 둘 다 되게 해놨음 # 파일리스트 중에서 DB만 뽑아오는 방법 찾는중 # 가져올 때 아예 검사해서 확장자 없는것만 가져오게 하는방법 # 혹은 가져오고 나서 검사해서 확장자 없는것만 출력하도록 하기

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 # 고객명 / ID(기본키) / PW / 나이 / 국적 / 거주지 / 가입날짜 / 등급
 
 import sqlite3
+import os
 from tkinter import *
 from functools import partial
 
 
-def _save_quit(a, b):
-    # DB 연결
-    # TestDB 대신에 Entry를 사용하여 사용자 입력으로 DB이름을 입력
-    # 입력받은 값으로 connect하여 DB 생성할 예정
-    print(b)
-    #con = sqlite3.connect("TestDB")
-    #cur = con.cursor()
-    #con.commit()
-    #con.close()
+def list_db():
+    root = Tk()
+    root.title("DB목록창")
+    root.geometry("300x300")
+
+    path_dir = 'D:/Users/kuk/OneDrive/01. repository/Python/DB_GUI/'
+    dblist = os.listdir(path_dir)
+    dblist.sort()
+
+    varT = StringVar()
+    varT.set(dblist)
+
+    label_dblist = Label(root, textvariable=varT)
+    label_dblist.pack(side=TOP)
+
+    root.mainloop()
+
+
+def save_quit(a, b, event):
+    con = sqlite3.connect(b.get())
+    cur = con.cursor()
+    con.commit()
+    con.close()
 
     a.destroy()  # 새로 열린 이 창만 닫기
 
@@ -23,25 +38,29 @@ def create_db():
     root.title("DB생성창")
     root.geometry("300x300")
 
-    # Entry 만들어서 DB이름 받고 그 값을 함수로 넘겨줄 것
-    test = StringVar()
-    label1 = Label(root, text='DB이름').pack(side=LEFT)
-    textbox = Entry(root, textvariable=test).pack(side=LEFT)
-    test = test.set('default')
-    save_quit_arg = partial(_save_quit, root, test)
-    savebtn = Button(root, text='save', command=save_quit_arg).pack(side=BOTTOM)
+    label_dbname = Label(root, text='DB이름')
+    label_dbname.pack(side=LEFT)
 
+    test = StringVar()
+    textbox_dbname = Entry(root, textvariable=test)
+    textbox_dbname.pack(side=LEFT)
+
+    save_quit_arg = partial(save_quit, root, textbox_dbname)
+    savebtn = Button(root, text='save')
+    savebtn.pack(side=BOTTOM)
+    savebtn.bind("<Button>", save_quit_arg)
+    root.bind("<Return>", save_quit_arg)
 
     root.mainloop()
 
 
 window = Tk()
 window.title("DB Manager v1.0")  # 창 이름
-window.geometry("200x450")  # 창 크기
+window.geometry("200x500")  # 창 크기
 window.resizable(width=TRUE, height=TRUE)  # 창 크기 변경 설정
 
 # 사용자 UI
-button1 = Button(window, text='DB목록')
+button1 = Button(window, text='DB목록', command=list_db)
 button2 = Button(window, text='DB생성', command=create_db)
 button3 = Button(window, text='DB삭제')
 button4 = Button(window, text='DB수정')
@@ -49,6 +68,7 @@ button5 = Button(window, text='Table목록')
 button6 = Button(window, text='Table생성')
 button7 = Button(window, text='Table삭제')
 button8 = Button(window, text='Table수정')
+button9 = Button(window, text='프로그램종료', command=exit)
 button1.place(x=40, y=30, width=100, height=30)
 button2.place(x=40, y=80, width=100, height=30)
 button3.place(x=40, y=130, width=100, height=30)
@@ -57,6 +77,7 @@ button5.place(x=40, y=230, width=100, height=30)
 button6.place(x=40, y=280, width=100, height=30)
 button7.place(x=40, y=330, width=100, height=30)
 button8.place(x=40, y=380, width=100, height=30)
+button9.place(x=40, y=430, width=100, height=30)
 window.mainloop()
 
 
